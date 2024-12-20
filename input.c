ac2a3c932d95287c6285d475312a7ddc8aa7032e
amenibelhaj <blameni073@gmail.com>
2024-12-20 14:15:40 +0200

    test

--- input.c
+++ input.c
@@ -3,16 +3,16 @@
  * get_line - Reads a line of input from the user.
  * @return: A pointer to the input string, or NULL if an error occurs.
  */
+
 char *get_line(void)
 {
 char *line = NULL;
 size_t len = 0;
+    
 if (getline(&line, &len, stdin) == -1)
 {
 free(line);
-exit(0);
+return (NULL);
 }
-
-line[strcspn(line, "\n")] = 0;
 return (line);
 }
